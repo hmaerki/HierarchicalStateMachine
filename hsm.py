ba92e1883dad36c3cdfc6122e7e0023280c7c021
Hans Märki <hans.maerki@zhinst.com>
2020-08-31 23:46:39 +0200

    All unit tests passed on python 3.7.

--- hsm.py
+++ hsm.py
@@ -1,8 +1,8 @@
-import inspect
-import cStringIO
+import io
 import re
+import inspect
 
-objRegexpSpaces = re.compile('^(?P<spaces>.*?)(\S(.*)$)', re.M)
+objRegexpSpaces = re.compile(r'^(?P<spaces>.*?)(\S(.*)$)', re.M)
 
 class StateChangeException(Exception):
   def __init__(self, methNewState):
@@ -32,16 +32,16 @@ class Statemachine:
 
   def setMiniLogger(self):
     def funcLogMain(strLine):
-      print strLine
+      print(strLine)
     def funcLogSub(strLine):
-      print '  ' + strLine
+      print(f'  {strLine}')
     self.funcLogMain = funcLogMain
     self.funcLogSub = funcLogSub
 
   def setLogger(self, funcLogMain, funcLogSub):
-    """
+    '''
       Tell the statemachine where to log
-    """
+    '''
     self.funcLogMain = funcLogMain
     self.funcLogSub = funcLogSub
 
@@ -50,26 +50,26 @@ class Statemachine:
 
     try:
       self.funcLogMain('%s: will be handled by %s' % (repr(objSignal), self.private_strStateActual))
-      self.funcLogSub('  calling state "state_%s(%s)"' % (strStateBefore, str(objSignal)))
+      self.funcLogSub(f'  calling state "state_{strStateBefore}({objSignal})"')
       strHandlingState = self.private_strStateActual
       while True:
         meth = getattr(self, 'state_' + strHandlingState)
         meth(objSignal)
         i = strHandlingState.rfind('_')
         if i<0:
-          raise Exception("Signal %s was not handled!" % str(objSignal))
-          # print "Empty Transition!"
+          raise Exception('Signal %s was not handled!' % str(objSignal))
+          # print('Empty Transition!')
           # return
         strHandlingState = strHandlingState[:i]
       return
-    except DontChangeStateException, e:
-      self.funcLogSub("  No state change!")
+    except DontChangeStateException as e:
+      self.funcLogSub('  No state change!')
       return
-    except IgnoreEventException, e:
-      self.funcLogSub("  Empty Transition!")
+    except IgnoreEventException as e:
+      self.funcLogSub('  Empty Transition!')
       return
-    except StateChangeException, e:
-      self.funcLogSub("%s: was handled by state_%s" % (str(objSignal), strHandlingState))
+    except StateChangeException as e:
+      self.funcLogSub('%s: was handled by state_%s' % (str(objSignal), strHandlingState))
       # Evaluate init-state
       strNewState = e.methNewState.__name__.replace('state_', '')
       strInitState = self.get_init_state(strNewState)
@@ -83,17 +83,16 @@ class Statemachine:
   def get_init_state(self, strState):
     try:
       return self.get_init_state(self.private_dictInitState[strState])
-    except KeyError, e:
+    except KeyError as _e:
       return strState
 
   def get_method_list(self, strType):
-    import inspect
     results = []
     for strKey in dir(self):
       objValue = getattr(self, strKey)
       if inspect.ismethod(objValue):
         # if objValue.__name__.find(strType) == 0:
-        if strKey.find(strType) == 0:
+        if strKey.startswith(strType):
           # results.append(objValue.__name__[len(strType):])
           results.append(strKey[len(strType):])
     results.sort()
@@ -115,7 +114,7 @@ class Statemachine:
       strState = '_'.join(listStateBefore[:i])
       try:
         fExit = getattr(self, 'exit_' + strState)
-      except AttributeError, e:
+      except AttributeError as _e:
         pass
       else:
         self.funcLogSub('  Calling %s' % (fExit.__name__))
@@ -125,7 +124,7 @@ class Statemachine:
       strState = '_'.join(listStateAfter[:i+1])
       try:
         fEntry = getattr(self, 'entry_' + strState)
-      except AttributeError, e:
+      except AttributeError as _e:
         pass
       else:
         self.funcLogSub('  Calling %s' % (fEntry.__name__))
@@ -148,7 +147,7 @@ class Statemachine:
         try:
           meth = getattr(self, 'state_' + strEntry)
           pass
-        except AttributeError, e:
+        except AttributeError as e:
           raise BadStatemachineException('No corresponding state_%s() for %s%s()!' % (strEntry, strAction, strEntry))
 
   def reset(self):
@@ -161,27 +160,21 @@ class Statemachine:
       objValue = getattr(self, 'init_' + strInitState)
       self.private_dictInitState[strInitState] = objValue.__name__[len('state_'):]
 
-    # self.get_top_state_obsolete(self.private_listStateNames)
-
     # Find top state
-    # self.strTopState = self.get_top_state_obsolete(self.private_listStateNames)
-    if not self.private_dictInitState.has_key(''):
+    if not '' in self.private_dictInitState:
       raise BadStatemachineException('Init-State on top-level is required but missing. Example "init_ = state_XYZ".')
-    # self.strTopState = self.private_dictInitState['']
     
     # Evaluate the init-state
-    # self.private_strStateActual = self.get_init_state(self.strTopState)
     self.private_strStateActual = self.get_init_state('')
     
     # Verify validity of the entry and exit actions
     self.consistency_check()
     
-    import inspect
     listMembers = inspect.getmembers(self)
     for strType, objValue in listMembers:
       if inspect.ismethod(objValue):
         pass
-        # print 'method: ' + strType + '->' + objValue.__name__
+        # print(f'method: {strType}->{objValue.__name__}')
     pass
 
   def start(self):
@@ -206,7 +199,7 @@ class Statemachine:
     if strDocString:
       # strDocString = strDocString.strip().replace('\n', '<br>')
       return self._beautify_docstring(strDocString)
-    return ""
+    return ''
 
   def get_hierarchy_(self, strParentState=''):
     listLevel = []
@@ -238,41 +231,41 @@ class Statemachine:
     return listLevel
 
   def doc_state(self, strState):
-    import cStringIO
-    f = cStringIO.StringIO()
+    f = io.StringIO()
     f.write('<table class="table_state">')
     f.write('  <tr>')
-    f.write('    <td class="td_header" colSpan="3">%s</td>' % strState.split('_')[-1])
+    text = strState.split('_')[-1]
+    f.write(f'    <td class="td_header" colSpan="3">{text}</td>')
     f.write('  </tr>')
-    strDocstring = self.get_docstring('state_'+strState)
+    strDocstring = self.get_docstring(f'state_{strState}')
     if strDocstring:
       f.write('<tr class="tr_comment">')
-      f.write('	<td class="td_space">&nbsp;&nbsp;&nbsp;</td>')
-      f.write('	<td class="td_label">comment</td>')
-      f.write('	<td class="td_text">%s</td>' % strDocstring)
+      f.write('  <td class="td_space">&nbsp;&nbsp;&nbsp;</td>')
+      f.write('  <td class="td_label">comment</td>')
+      f.write(f'  <td class="td_text">{strDocstring}</td>')
       f.write('</tr>')
     if strState in self.private_listInitNames:
       strInitState = self.private_dictInitState[strState]
       f.write('<TR class="tr_init">')
-      f.write('	<TD></TD>')
-      f.write('	<TD class="td_label">init</TD>')
-      f.write('	<TD class="td_text">%s</TD>' % strInitState)
+      f.write('  <TD></TD>')
+      f.write('  <TD class="td_label">init</TD>')
+      f.write(f'  <TD class="td_text">{strInitState}</TD>')
       f.write('</TR>')
     if strState in self.private_listEntryNames:
       strDocstring = self.get_docstring('entry_'+strState)
       if strDocstring:
         f.write('<TR class="tr_entry">')
-        f.write('	<TD></TD>')
-        f.write('	<TD class="td_label">entry</TD>')
-        f.write('	<TD class="td_text">%s</TD>' % strDocstring)
+        f.write('  <TD></TD>')
+        f.write('  <TD class="td_label">entry</TD>')
+        f.write(f'  <TD class="td_text">{strDocstring}</TD>')
         f.write('</TR>')
     if strState in self.private_listExitNames:
       strDocstring = self.get_docstring('exit_'+strState)
       if strDocstring:
         f.write('<TR class="tr_exit">')
-        f.write('	<TD></TD>')
-        f.write('	<TD class="td_label">exit</TD>')
-        f.write('	<TD class="td_text">%s</TD>' % strDocstring)
+        f.write('  <TD></TD>')
+        f.write('  <TD class="td_label">exit</TD>')
+        f.write(f'  <TD class="td_text">{strDocstring}</TD>')
         f.write('</TR>')
     if False:
       bShowSource = False
@@ -280,32 +273,31 @@ class Statemachine:
         strSource = inspect.getsource(getattr(self, 'state_'+strState))
         if not strSource:
           strSource = '&nbsp;'
-        f.write('<td valign="top" height="100%"><pre class="small">' + strSource + '</pre></td>')
+        f.write(f'<td valign="top" height="100%"><pre class="small">{strSource}</pre></td>')
     return f.getvalue(), '</table>'
 
 
   def doc_state_recursive(self, listStates):
-    f = cStringIO.StringIO()
+    f = io.StringIO()
     for strState, listStates in listStates:
       strStateBegin, strStateEnd = self.doc_state(strState)
       strStateSub = self.doc_state_recursive(listStates)
       f.write(strStateBegin)
       if strStateSub:
         f.write('  <tr class="tr_sub">')
-        f.write('  	<td class="td_space">&nbsp;&nbsp;&nbsp;</td>')
-        f.write('  	<td class="td_substate" colSpan="2">')
+        f.write('    <td class="td_space">&nbsp;&nbsp;&nbsp;</td>')
+        f.write('    <td class="td_substate" colSpan="2">')
         f.write(strStateSub)
-        f.write('  	</td>')
+        f.write('    </td>')
         f.write('  </tr>')
       f.write(strStateEnd)
     return f.getvalue()
 
   def doc(self):
-    """
+    '''
       Return a HTML-Page which describes the HSM
-    """
-    bShowSource = False
-    f = cStringIO.StringIO()
+    '''
+    f = io.StringIO()
     f.write(html_header)
     strDocstring = inspect.getdoc(self)
     if strDocstring:
@@ -317,52 +309,49 @@ class Statemachine:
     f.write('</html>\n')
     return f.getvalue()
 
-html_header = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?xml version="1.0" encoding="iso-8859-1" ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Hierarchical State Machine</title> 
-		<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
-		<meta content="Zulu Website-Assembler v2.1.3" name="assembler">
-		<meta content="Zulu by Positron and Maerki Informatik" name="description">
-		<meta content="Zulu Website Assembler Positron Maerki Informatik Schweiz Switzerland"
-			name="keywords">
-		<style type="text/css">
-		<!--
-		/*  common styles  */
-		table.table_state {width: 100%; border-left: 2px solid #000000; border-right: 2px solid #000000; border-top: 0px solid #000000; border-bottom: 0px solid #000000}
-		td.td_header { background-color: #EEEEEE; border-bottom: 1px solid #000000; border-top: 1px solid #000000; font-weight: bold}
-		td.td_label {width: 1%; font-size: smaller; font-style: italic}
-		td.td_text {font-size: smaller}
-		td.td_space {width: 1%}
-		td.td_substate {width: 100%}
-		-->
-		</style>
-	</head>
-	<body>
-"""
+html_header = '''
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<?xml version='1.0' encoding='iso-8859-1' ?>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+  <head>
+    <title>Hierarchical State Machine</title> 
+    <meta http-equiv='content-type' content='text/html;charset=iso-8859-1'>
+    <meta content='HSM by Positron and Maerki Informatik' name='description'>
+    <style type='text/css'>
+    <!--
+    /*  common styles  */
+    table.table_state {width: 100%; border-left: 2px solid #000000; border-right: 2px solid #000000; border-top: 0px solid #000000; border-bottom: 0px solid #000000}
+    td.td_header { background-color: #EEEEEE; border-bottom: 1px solid #000000; border-top: 1px solid #000000; font-weight: bold}
+    td.td_label {width: 1%; font-size: smaller; font-style: italic}
+    td.td_text {font-size: smaller}
+    td.td_space {width: 1%}
+    td.td_substate {width: 100%}
+    -->
+    </style>
+  </head>
+  <body>
+'''
 
 #class Test_NoTopState(Statemachine):
-#  """
+#  '''
 #    >>> sm = Test_NoTopState()
 #    >>> sm.reset()
 #    Traceback (most recent call last):
 #    ...
 #    BadStatemachineException: No Top State found
-#  """
+#  '''
 #  def state_TopA_SubB(self, objSignal):
 #    pass
 #  init_ = state_TopA_SubB
 
 class Test_UnmatchedEntryAction(Statemachine):
-  """
+  '''
     >>> sm = Test_UnmatchedEntryAction()
     >>> sm.reset()
     Traceback (most recent call last):
     ...
-    BadStatemachineException: No corresponding state_TopA_SubB() for entry_TopA_SubB()!
-  """
+    hsm.BadStatemachineException: No corresponding state_TopA_SubB() for entry_TopA_SubB()!
+  '''
   def state_TopA(self, objSignal):
     pass
   def entry_TopA_SubB(self, objSignal):
@@ -370,13 +359,13 @@ class Test_UnmatchedEntryAction(Statemachine):
   init_ = state_TopA
 
 class Test_UnmatchedExitAction(Statemachine):
-  """
+  '''
     >>> sm = Test_UnmatchedExitAction()
     >>> sm.reset()
     Traceback (most recent call last):
     ...
-    BadStatemachineException: No corresponding state_TopA_SubB() for exit_TopA_SubB()!
-  """
+    hsm.BadStatemachineException: No corresponding state_TopA_SubB() for exit_TopA_SubB()!
+  '''
   def state_TopA(self, objSignal):
     pass
   def exit_TopA_SubB(self):
@@ -384,7 +373,7 @@ class Test_UnmatchedExitAction(Statemachine):
   init_ = state_TopA
 
 class Test_SimpleStatemachineTopStateHandlesSignal(Statemachine):
-  """
+  '''
     >>> sm = Test_SimpleStatemachineTopStateHandlesSignal()
     >>> sm.reset()
     >>> sm.setMiniLogger()
@@ -392,18 +381,18 @@ class Test_SimpleStatemachineTopStateHandlesSignal(Statemachine):
     'a': will be handled by TopA_SubA
         calling state "state_TopA_SubA(a)"
         No state change!
-  """
+  '''
   def state_TopA(self, objSignal):
-    if (objSignal == 'a'):
+    if objSignal == 'a':
       raise DontChangeStateException
   def state_TopA_SubA(self, objSignal):
-    if (objSignal == 'b'):
-      raise StateChangeException(self.state_TopA_SubB)
+    if objSignal == 'b':
+      raise StateChangeException(self.state_TopA_SubA)
   init_ = state_TopA_SubA
 
 
 class Test_SimpleStatemachine(Statemachine):
-  """
+  '''
     >>> sm = Test_SimpleStatemachine()
     >>> sm.reset()
     >>> sm.setMiniLogger()
@@ -425,13 +414,13 @@ class Test_SimpleStatemachine(Statemachine):
         calling state "state_TopA_SubB(a)"
       a: was handled by state_TopA
         Calling exit_TopA_SubB
-  """
+  '''
   def state_TopA(self, objSignal):
-    if (objSignal == 'a'):
+    if objSignal == 'a':
       raise StateChangeException(self.state_TopA_SubA)
     raise IgnoreEventException()
   def state_TopA_SubA(self, objSignal):
-    if (objSignal == 'b'):
+    if objSignal == 'b':
       raise StateChangeException(self.state_TopA_SubB)
   def state_TopA_SubB(self, objSignal):
     pass
@@ -442,7 +431,7 @@ class Test_SimpleStatemachine(Statemachine):
   init_ = state_TopA
 
 class Test_StatemachineWithEntryExitActions(Statemachine):
-  """
+  '''
     >>> sm = Test_StatemachineWithEntryExitActions()
     >>> sm.reset()
     >>> sm.setMiniLogger()
@@ -472,7 +461,7 @@ class Test_StatemachineWithEntryExitActions(Statemachine):
     Traceback (most recent call last):
     ...
     Exception: Signal c was not handled!
-  """
+  '''
   def state_TopA(self, objSignal):
     raise StateChangeException(self.state_TopC)
   def exit_TopA(self):
@@ -508,5 +497,5 @@ def test():
   import doctest, hsm
   return doctest.testmod(hsm)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   test()
--- test_hsm.py
+++ test_hsm.py
@@ -1,92 +1,87 @@
 import hsm
 
 class test_hsm(hsm.Statemachine):
-  """
+  '''
     This is a sample Statemachine as in figure 6.2 on page 170
-    in "Practical Statecharts in C/C++", ISBN 1-57820-110-1.
-  """
+    in 'Practical Statecharts in C/C++', ISBN 1-57820-110-1.
+  '''
   def __init__(self):
-    hsm.Statemachine.__init__(self)
+    super().__init__()
     self.foo = False
 
   def state_0(self, objSignal):
-    "This is State '0'!"
-    if (objSignal == 'E'):
+    'This is State "0"!'
+    if objSignal == 'E':
       raise hsm.StateChangeException(self.state_0_2_1_1)
-    if (objSignal == 'I'):
+    if objSignal == 'I':
       raise hsm.StateChangeException(self.state_0)
-    if (objSignal == 'J'):
+    if objSignal == 'J':
       raise hsm.IgnoreEventException()
   
   def state_0_1(self, objSignal):
-    if (objSignal == 'A'):
+    if objSignal == 'A':
       raise hsm.StateChangeException(self.state_0_1)
-    if (objSignal == 'C'):
+    if objSignal == 'C':
       raise hsm.StateChangeException(self.state_0_2)
-    if (objSignal == 'D'):
+    if objSignal == 'D':
       raise hsm.StateChangeException(self.state_0)
-    if (objSignal == 'E'):
+    if objSignal == 'E':
       raise hsm.StateChangeException(self.state_0_2_1_1)
 
   def state_0_1_1(self, objSignal):
-    if (objSignal == 'G'):
+    if objSignal == 'G':
       raise hsm.StateChangeException(self.state_0_2_1_1)
-    if (objSignal == 'H'):
-      if (self.foo):
+    if objSignal == 'H':
+      if self.foo:
         self.foo = False
   
   def state_0_2(self, objSignal):
-    if (objSignal == 'C'):
+    if objSignal == 'C':
       raise hsm.StateChangeException(self.state_0_1)
-    if (objSignal == 'F'):
+    if objSignal == 'F':
       raise hsm.StateChangeException(self.state_0_1_1)
   
   def state_0_2_1(self, objSignal):
-    if (objSignal == 'B'):
+    if objSignal == 'B':
       raise hsm.StateChangeException(self.state_0_2_1_1)
-    if (objSignal == 'H'):
-      if (not self.foo):
+    if objSignal == 'H':
+      if not self.foo:
         self.foo = True
         raise hsm.StateChangeException(self.state_0_2_1)
 
   def state_0_2_1_1(self, objSignal):
-    if (objSignal == 'C'):
+    if objSignal == 'C':
       raise hsm.StateChangeException(self.state_0_2_2)
-    if (objSignal == 'D'):
+    if objSignal == 'D':
       raise hsm.StateChangeException(self.state_0_2_1)
-    if (objSignal == 'G'):
+    if objSignal == 'G':
       raise hsm.StateChangeException(self.state_0)
   
   def state_0_2_2(self, objSignal):
-    if (objSignal == 'B'):
+    if objSignal == 'B':
       raise hsm.IgnoreEventException()
-    if (objSignal == 'G'):
+    if objSignal == 'G':
       raise hsm.StateChangeException(self.state_0_2_1)
 
-  def entry_0(self):
+  def entry_0(self, objSignal):
     pass
   
   def exit_0(self):
     pass
   
-  def entry_0_1(self):
+  def entry_0_1(self, objSignal):
     pass
   
   def exit_0_1(self):
     pass
   
-  def entry_0_1_1(self):
+  def entry_0_1_1(self, objSignal):
     pass
   
+  init_ = state_0
   init_0 = state_0_1
   init_0_1 = state_0_1_1
 
-
-  def testEntryExit(sm, a, b, c, d, e):
-    pass
-  
-  def testTransition(sm, objSignal, newState):
-    sm.dispatch(objSignal)
 
 def analyse():
   def funcLogMain(strLine):
@@ -144,24 +139,11 @@ def analyse():
   testTransition(sm, 'J', sm.state_0_1_1)
   testEntryExit(sm, 0, 0, 0, 0, 0)
 
-  print "\nIf you got here, the statemachine seems to work ok!\n\n"
+  print('\nIf you got here, the statemachine seems to work ok!\n\n')
 
-  print sm.doc()
+  print(sm.doc())
   open('test_hsm_out.html', 'w').write(sm.doc())
 
-  return
 
-  print sm.__dict__
-  print inspect.isclass(sm)
-  listMembers = inspect.getmembers(sm)
-  for strType, objValue in listMembers:
-    if inspect.ismethod(objValue):
-      print 'method: ' + strType + '->' + objValue.__name__
-  for x in listMembers:
-    print x
-
-if __name__ == "__main__":
+if __name__ == '__main__':
   analyse()
-  
-
-
